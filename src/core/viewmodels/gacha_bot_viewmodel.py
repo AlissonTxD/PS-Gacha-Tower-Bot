35eb7b3576305b3ffdcdd41c915be4ab20587256
Alisson Notebook <omigadortxd@gmail.com>
2026-09-18 13:36:49 -0400

    fix: update gacha replay recording method to use the correct attribute

--- src/core/viewmodels/gacha_bot_viewmodel.py
+++ src/core/viewmodels/gacha_bot_viewmodel.py
@@ -49,7 +49,7 @@ class GachaBotViewModel:
                 self.__rest(3)
         except Exception as e:  # noqa: BLE001
             #record
-            self.gacha.record_replay()
+            self.gacha.gt.record_replay()
             logging.warning(e)  # noqa: LOG015
 
     def __collect_crystals(self) -> None:
